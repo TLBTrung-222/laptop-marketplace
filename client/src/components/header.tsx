fc569b3e3908a335ed04c9c30a06025ab13bf855
bluewhale <lqtu12369@gmail.com>
2024-11-15 10:40:50 +0700

    feat: add favicon

---
+++ client/src/components/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <div className="fixed top-0 left-0 right-0 container xl:max-w-screen-xl mx-2 sm:mx-auto ">
+            Header
+        </div>
+    );
+};
